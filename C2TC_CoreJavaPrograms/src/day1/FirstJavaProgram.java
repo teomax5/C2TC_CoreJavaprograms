18e582a5c88c862329291c3ad4949243a944f512
teomax5 <73957210+teomax5@users.noreply.github.com>
2025-06-12 12:40:34 +0530

    program for adding two numbers

--- C2TC_CoreJavaPrograms/src/day1/FirstJavaProgram.java
+++ C2TC_CoreJavaPrograms/src/day1/FirstJavaProgram.java
@@ -1,9 +1,18 @@
 package day1;
-
+import java.util.Scanner;
 public class FirstJavaProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Java");
+		int a,b,c;
+		Scanner sc;
+		sc= new Scanner(System.in);
+		System.out.println("Enter a:");
+		a=sc.nextInt();
+		System.out.println("Enter b:");
+		b=sc.nextInt();
+		c=a+b;
+	System.out.println("Addition 0f a="+a+"and b="+b+" is "+c);	
 
 	}
 
