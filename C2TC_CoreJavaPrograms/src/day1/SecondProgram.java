9266087d69ffac6f1eeb5149f40a128ee73300f1
teomax5 <73957210+teomax5@users.noreply.github.com>
2025-06-13 10:35:42 +0530

    java programs

--- C2TC_CoreJavaPrograms/src/day1/SecondProgram.java
+++ C2TC_CoreJavaPrograms/src/day1/SecondProgram.java
@@ -3,7 +3,25 @@ package day1;
 public class SecondProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// assigning single character literal
+		char ch='a';
+		System.out.println(ch);
+		
+		//assigning number to char
+		char ch1=65;
+		System.out.println(ch1);
+		
+		//assigning unicode to char
+		char var1='\u00A7';
+		System.out.println(var1);
+		
+		//unicode representation
+		char var2='\u20AC';
+		System.out.println(var2);
+		
+		//ASCII code representation
+		int a='A';
+		System.out.println(a);
 
 	}
 
