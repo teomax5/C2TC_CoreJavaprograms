225b7ab3cc0a9a82d9bf97a8e07423dc8e7eb9e2
teomax5 <73957210+teomax5@users.noreply.github.com>
2025-06-13 12:32:03 +0530

    jc

---
+++ C2TC_CoreJavaPrograms/src/demo/studentdemo.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class studentdemo {
+
+	public static void main(String[] args) {
+		student st=new student();
+		st.setId(101);
+		st.setName("Helan");
+		st.setAge(20);
+		System.out.println(st);
+		
+		student st1=new student();
+		st1.setId(102);
+		st1.setName("Mary");
+		st1.setAge(21);
+		System.out.println(st1);
+		
+        
+	}
+
+}
