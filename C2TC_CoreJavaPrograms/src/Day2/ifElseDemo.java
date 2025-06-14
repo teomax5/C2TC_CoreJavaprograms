f7cec705eec38f90f458218c844929e5891a7783
teomax5 <73957210+teomax5@users.noreply.github.com>
2025-06-14 22:44:23 +0530

    Program for java Day2

---
+++ C2TC_CoreJavaPrograms/src/Day2/ifElseDemo.java
@@ -0,0 +1,17 @@
+package Day2;
+
+public class ifElseDemo {
+	public static void main(String[] args) {
+		int age=11;
+		
+		if(age>=18)
+			System.out.println(" You are eligible for Voting");	
+		else
+			System.out.println("Sorry! You need to wait more");
+
+		System.out.println("if else demonstration");
+		
+	}
+
+
+}
